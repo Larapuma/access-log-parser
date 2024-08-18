feb043ccfffde42ffb3ff48dc5e24a2ee806aa82
Totoro <N1.plechkov1@gmail.com>
2024-08-18 19:21:50 +0300

    Ez

--- src/main/java/cycles/Main.java
+++ src/main/java/cycles/Main.java
@@ -1,2 +1,24 @@
-package cycles;public class Main {
+package cycles;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (true){
+            System.out.println("Введите путь: ");
+            String path = new Scanner(System.in).nextLine();
+             File file = new File(path);
+             boolean fileExists = file.exists();
+             boolean isDirectory = file.isDirectory();
+             if(!fileExists|| isDirectory){
+                 System.out.println("Путь указан неверно.");
+                 continue;
+             }
+            System.out.println("Путь указан верно");
+             counter +=1;
+            System.out.println("Это файл номер " + counter + ".");
+        }
+    }
 }
