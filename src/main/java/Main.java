e26b22a1c150c9de776972af952114fc07aa9b6c
Totoro <N1.plechkov1@gmail.com>
2024-08-18 20:06:28 +0300

    Ne ebu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите число: ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите число: ");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма = "+number1 + " + " + number2 + " = " + (number2+number1));
+        System.out.println("Разность = "+ number1 + " - " + number2 + " = " + (number1+number2));
+        System.out.println("Произведение = "+number1 + " * " + number2 + " = " + (number2*number1));
+
+        double quotient = (double) number1/number2;
+        System.out.println("Отношение = "+number1 + " / " + number2 + " = " + (quotient));
+
+
+
     }
 }
